147586bd379e766c789aba1f2ca107235a076fc7
Gabriel Higino <higinogames@gmail.com>
2025-09-19 13:32:54 -0300

    working with four verbs

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/obj/Release/net8.0/RestWithASPNETUdemy.AssemblyInfo.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/obj/Release/net8.0/RestWithASPNETUdemy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNETUdemy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5312433fc63224764262db327b179a2250d1dfe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithASPNETUdemy")]
